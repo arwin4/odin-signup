aa8f82af74b8bc08f60feda73b7074e5e38fd40f
Arwin Yella <16850195+arwin4@users.noreply.github.com>
2023-03-06 11:50:16 +0100

    Add js check for password match

---
+++ main.js
@@ -0,0 +1,23 @@
+function checkPasswordMatch(password2) {
+  let password1 = document.querySelector('#password').value;
+  const warningText = document.querySelector('.password-warning');
+
+  // Only check passwords when both meet requirements
+  if ((password1.length && password2.length) < 8) return;
+
+  if (password1 !== password2) {
+    warningText.textContent = 'Passwords do not match.';
+  } else {
+    warningText.textContent = '';
+  }
+}
+
+const password1Field = document.querySelector('#password');
+password1Field.addEventListener('keyup', () => {
+  checkPasswordMatch(password2Field.value);
+});
+
+const password2Field = document.querySelector('#confirm-password');
+password2Field.addEventListener('keyup', () => {
+  checkPasswordMatch(password2Field.value);
+});
